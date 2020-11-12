192e8b9fbda874dacfbbe0f8918b46bc63c410dd
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:16:12 +0200

    Add project files.

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+
+            string[] AllDirectories = Directory.GetDirectories(rootPath);
+            //ReadArray(AllDirectories);
+
+            string[] includeNestedDirectories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            // ReadArray(includeNestedDirectories);
+
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+
+
+        }
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+
+    }
+}
---
+++ WriteFilesAndDirectories/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\opilanesamples\files.txt";
+            string directorypath = @"C:\Users\opilane\samples";
+
+            string[] files = Directory.GetFiles(directorypath, "*.*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(filepath, files);
+            File.WriteAllLines(fileTxt, allFiles);
+
+
+        }
+        public static void
+    }
+}
